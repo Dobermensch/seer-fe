71be56daab554e2d21750bdcff34c197fcbd7704
dobermensch <dobermensch@gmail.com>
2024-03-31 12:24:30 +0800

    refactor: sort useStates & add select component

---
+++ src/app/components/RPSLSSelect.js
@@ -0,0 +1,17 @@
+"use client"
+import styles from "../page.module.css"
+
+export default function RPSLSSelect({ label, labelText, onChangeHandler }) {
+  return (
+    <>
+      <label htmlFor={label}>{labelText}: </label>
+      <select className={styles.input} onChange={onChangeHandler} type="select" id={label} name={label}>
+          <option value="1">Rock</option>
+          <option value="2">Paper</option>
+          <option value="3">Scissors</option>
+          <option value="4">Lizard</option>
+          <option value="5">Spock</option>
+      </select>
+    </>
+  )
+}
