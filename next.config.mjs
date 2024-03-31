7f171b0ee4dc59b423264e1e7c365fd0f338d254
dobermensch <dobermensch@gmail.com>
2024-03-31 17:22:52 +0800

    add: backend api link

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_ENDPOINT: "http://localhost:3001"
+    NEXT_PUBLIC_API_ENDPOINT: process.env.NODE_ENV === "production" ? "https://seer-be-2110dd3ebd6f.herokuapp.com" : "http://localhost:3001"
   }
 };
 
